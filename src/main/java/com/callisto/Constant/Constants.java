741d8dc75abddddec7e48c919ac1378d77f04529
kumar_su <kumar_su@hcltech.com>
2025-05-21 14:41:08 +0530

    Updated pom.xml, application.properties file database connection and added validation

--- src/main/java/com/callisto/Constant/Constants.java
+++ src/main/java/com/callisto/Constant/Constants.java
@@ -1,6 +1,6 @@
 package com.callisto.Constant;
 
 public class Constants {
-    public static String firstNameNotFound = "First name is mandatory.";
+    public static String firstNameNotFound = "Firstname is mandatory.";
     public static String emailNotFound = "Email already registered.";
 }
