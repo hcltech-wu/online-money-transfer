f84a079ab404256579499e1f24829de00ae31275
kumar_su <kumar_su@hcltech.com>
2025-05-23 16:25:18 +0530

    resolved unnecessary import

--- src/main/java/com/callisto/controller/UserController.java
+++ src/main/java/com/callisto/controller/UserController.java
@@ -13,7 +13,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
--- src/main/java/com/callisto/dto/CustomerProfileDTO.java
+++ src/main/java/com/callisto/dto/CustomerProfileDTO.java
@@ -1,6 +1,10 @@
 package com.callisto.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
