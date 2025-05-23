b944e86e895173a6ab3738a8f08d9758ba15a974
sanketb9665 <sanket.badhekar@hcltech.com>
2025-05-23 12:26:10 +0530

    before merge

--- src/main/java/com/callisto/controller/UserController.java
+++ src/main/java/com/callisto/controller/UserController.java
@@ -1,19 +1,15 @@
 package com.callisto.controller;
 
-<<<<<<< HEAD
 import com.callisto.Constant.LogMessages;
 import com.callisto.dto.CustomerProfileDTO;
 
 import com.callisto.service.UserService;
 import lombok.extern.log4j.Log4j2;
-=======
 import com.callisto.Constant.SmConstants;
 import com.callisto.model.User;
-import com.callisto.service.UserService;
 import jakarta.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
->>>>>>> a265d9b76f095a9cfe4e15bbf1077d487cc0a063
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,19 +22,19 @@ public class UserController {
     private static final Logger logger = LogManager.getLogger(UserController.class);
     @Autowired
     private UserService userService;
-
-<<<<<<< HEAD
+    
     @GetMapping("/userProfile")
     public ResponseEntity<CustomerProfileDTO> getCustomerProfile(@RequestParam String email) {
         log.info(LogMessages.REQUEST_RECEIVED_EMAIL, email);
         CustomerProfileDTO profile = userService.getCustomerProfileByEmail(email);
         return ResponseEntity.ok(profile);
-=======
+    }
+
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@Valid @RequestBody User user) {
         logger.info("Received request to /api/user/register with params: {}", user);
         userService.saveUser(user);
         return ResponseEntity.ok(SmConstants.USER_REGISTERED);
->>>>>>> a265d9b76f095a9cfe4e15bbf1077d487cc0a063
+
     }
 }
--- src/main/java/com/callisto/service/UserService.java
+++ src/main/java/com/callisto/service/UserService.java
@@ -1,26 +1,18 @@
 package com.callisto.service;
 
-<<<<<<< HEAD
 import com.callisto.Constant.ExceptionConstants;
 import com.callisto.Constant.LogMessages;
 import com.callisto.dto.CustomerProfileDTO;
-=======
 import com.callisto.controller.UserController;
->>>>>>> a265d9b76f095a9cfe4e15bbf1077d487cc0a063
 import com.callisto.exception.FirstNameNotFoundException;
 import com.callisto.exception.ResourceNotFoundException;
 import com.callisto.exception.UserEmailNotFoundException;
 import com.callisto.model.Address;
 import com.callisto.model.User;
 import com.callisto.repository.UserRepository;
-<<<<<<< HEAD
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
-=======
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
->>>>>>> a265d9b76f095a9cfe4e15bbf1077d487cc0a063
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -39,7 +31,8 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-<<<<<<< HEAD
+  private static final Logger logger = LogManager.getLogger(UserController.class);
+
     public CustomerProfileDTO getCustomerProfileByEmail(String email) {
         log.info(LogMessages.FETCHING_USER_BY_EMAIL, email);
         User user = userRepository.findByEmail(email)
@@ -55,10 +48,12 @@ public class UserService {
         final String status = "Active";
         return new CustomerProfileDTO(fullName, user.getDob(), addressString, LogMessages.language, LogMessages.status,
                 user.getEmail(), mobile);
-=======
-    private static final Logger logger = LogManager.getLogger(UserController.class);
+    }
+
 
     public void saveUser(User user) {
+
+        }
         logger.info("Inside saveUser()");
         logger.info("Saving user with Email: {}", user.getEmail());
         if (userRepository.existsByEmail(user.getEmail())) {
@@ -82,6 +77,5 @@ public class UserService {
         userRepository.save(user);
 
         logger.info("user saved successfully: {}" + user.getEmail());
->>>>>>> a265d9b76f095a9cfe4e15bbf1077d487cc0a063
     }
 }
